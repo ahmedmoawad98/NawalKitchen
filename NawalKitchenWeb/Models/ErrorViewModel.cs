fa55d9a9b183591a2431ed67b12251b3d1ab20f4
Ahmed Moawad <ahmedmoawad9823@gmail.com>
2022-02-07 16:25:02 -0500

    Add project files.

---
+++ NawalKitchenWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NawalKitchenWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
