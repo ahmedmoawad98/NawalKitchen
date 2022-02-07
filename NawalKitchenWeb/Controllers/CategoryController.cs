c488babf72b817fdd426b90ef5365cd3864b60ea
Ahmed Moawad <ahmedmoawad9823@gmail.com>
2022-02-07 18:02:45 -0500

    created table for category to display data

---
+++ NawalKitchenWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NawalKitchenWeb.Data;
+using NawalKitchenWeb.Models;
+
+namespace NawalKitchenWeb.Controllers;
+
+public class CategoryController : Controller
+{
+    private readonly ApplicationDbContext _db;
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    public IActionResult Index()
+    {
+        IEnumerable<Category> objCategoryList = _db.Categories;
+        return View(objCategoryList);
+    }
+}
+
