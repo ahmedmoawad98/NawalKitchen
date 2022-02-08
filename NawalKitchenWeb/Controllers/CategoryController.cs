4e57d92b38801e4898af1b31ef56f41823ecffcf
Ahmed Moawad <ahmedmoawad9823@gmail.com>
2022-02-08 10:05:30 -0500

    added alerts

--- NawalKitchenWeb/Controllers/CategoryController.cs
+++ NawalKitchenWeb/Controllers/CategoryController.cs
@@ -35,6 +35,7 @@ public class CategoryController : Controller
         {
             _db.Categories.Add(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category created successfully";
             return RedirectToAction("Index");
         }
         return View(obj);   
@@ -70,6 +71,7 @@ public class CategoryController : Controller
         {
             _db.Categories.Update(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category updated successfully";
             return RedirectToAction("Index");
         }
         return View(obj);
@@ -104,7 +106,8 @@ public class CategoryController : Controller
         }
             _db.Categories.Remove(obj);
             _db.SaveChanges();
-            return RedirectToAction("Index");
+        TempData["success"] = "Category deleted successfully";
+        return RedirectToAction("Index");
     }
 }
 
