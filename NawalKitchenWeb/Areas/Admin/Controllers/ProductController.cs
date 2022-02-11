0cf462a51103f54b6bc9af408af37297a90cee93
Ahmed Moawad <ahmedmoawad9823@gmail.com>
2022-02-11 08:31:27 -0500

    added view for edit product

--- NawalKitchenWeb/Areas/Admin/Controllers/ProductController.cs
+++ NawalKitchenWeb/Areas/Admin/Controllers/ProductController.cs
@@ -20,46 +20,27 @@ public class ProductController : Controller
         IEnumerable<CoverType> objCoverTypeList = _unitOfWork.CoverType.GetAll();
         return View(objCoverTypeList);
     }
+    
     //GET
-    public IActionResult Create()
-    {    
-        return View();
-    }
-
-    //POST
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public IActionResult Create(CoverType obj)
+    public IActionResult Upsert(int? id)
     {
-        if (ModelState.IsValid)
-        {
-            _unitOfWork.CoverType.Add(obj);
-            _unitOfWork.Save();
-            TempData["success"] = "CoverType created successfully";
-            return RedirectToAction("Index");
-        }
-        return View(obj);   
-    }
-
-    //GET
-    public IActionResult Edit(int? id)
-    {
+        Product product = new();
         if(id == null || id == 0)
         {
-            return NotFound();
+            //create product
+            return View(product);
         }
-        var CoverTypeFromDbFirst = _unitOfWork.CoverType.GetFirstOrDefault(u=>u.Id == id);
-        if (CoverTypeFromDbFirst == null)
+        else
         {
-            return NotFound();
+            //update product
         }
-        return View(CoverTypeFromDbFirst);
+        return View(product);
     }
 
     //POST
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public IActionResult Edit(CoverType obj)
+    public IActionResult Upsert(CoverType obj)
     {
         if (ModelState.IsValid)
         {
