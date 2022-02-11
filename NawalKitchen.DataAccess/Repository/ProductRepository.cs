5d33d6dab19062742ace87bda8b199a6ca61f064
Ahmed Moawad <ahmedmoawad9823@gmail.com>
2022-02-11 07:47:49 -0500

    added product repo

---
+++ NawalKitchen.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,37 @@
+﻿using NawalKitchen.DataAccess.Repository.IRepository;
+using NawalKitchen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NawalKitchen.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ProductRepository(ApplicationDbContext db): base(db)
+        {
+            _db = db;
+        }
+        
+
+        public void Update(Product obj)
+        {
+            var objFromDb = _db.Products.FirstOrDefault(u=>u.Id== obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = obj.Name;
+                objFromDb.Description = obj.Description;
+                objFromDb.Price = obj.Price;
+                objFromDb.CategoryId = obj.CategoryId;
+                if(obj.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;
+                }
+            }
+        }
+    }
+}
