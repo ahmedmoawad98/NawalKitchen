fdb69840052bfe1810b67f7378dfcf02510e0503
Ahmed Moawad <ahmedmoawad9823@gmail.com>
2022-02-11 07:35:31 -0500

    added product table

---
+++ NawalKitchen.Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NawalKitchen.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required]
+        [Range(1,100)]
+        public double Price { get; set; }   
+        public string ImageUrl { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }  
+    }
+}
