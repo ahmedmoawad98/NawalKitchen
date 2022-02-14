f3905cf44b28cbf0819748354ef88502b696bc49
Ahmed Moawad <ahmedmoawad9823@gmail.com>
2022-02-14 15:36:25 -0500

    added product to table

--- NawalKitchen.Models/Product.cs
+++ NawalKitchen.Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -15,12 +16,14 @@ namespace NawalKitchen.Models
         public string Name { get; set; }
         public string Description { get; set; }
         [Required]
-        [Range(1,100)]
-        public double Price { get; set; }   
+        [Range(1, 100)]
+        public double Price { get; set; }
+        [ValidateNever]
         public string ImageUrl { get; set; }
         [Required]
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
+        [ValidateNever]
         public Category Category { get; set; }  
     }
 }
