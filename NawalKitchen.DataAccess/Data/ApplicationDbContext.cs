bbf8c9f07a1eea22493ef26e39472a4832fd645a
Ahmed Moawad <ahmedmoawad9823@gmail.com>
2022-02-17 10:15:35 -0500

    added shopping cart to db

--- NawalKitchen.DataAccess/Data/ApplicationDbContext.cs
+++ NawalKitchen.DataAccess/Data/ApplicationDbContext.cs
@@ -17,5 +17,6 @@ public class ApplicationDbContext: IdentityDbContext
     public DbSet<Product> Products { get; set; }
     public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     public DbSet<Company> Companies { get; set; }
+    public DbSet<ShoppingCart> ShoppingCarts { get; set; }
 }
 
