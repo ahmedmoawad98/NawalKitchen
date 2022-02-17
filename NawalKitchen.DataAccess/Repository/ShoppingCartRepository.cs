eec4b827d59d42813e464e1ac533d9b6c7f7c8b4
Ahmed Moawad <ahmedmoawad9823@gmail.com>
2022-02-17 10:25:52 -0500

    added shopping cart and application user to repo

---
+++ NawalKitchen.DataAccess/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,21 @@
+﻿using NawalKitchen.DataAccess.Repository.IRepository;
+using NawalKitchen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NawalKitchen.DataAccess.Repository
+{
+    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ShoppingCartRepository(ApplicationDbContext db): base(db)
+        {
+            _db = db;
+        }
+        
+    }
+}
